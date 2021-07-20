9309e5b39bfee03afa1fdd7737d9ae813beafa0f
M.B Shuvo <mbshuvo2255@gmail.com>
2021-07-20 12:27:04 +0600

    Connect seller & product Api

--- lib/home/presentation/manager/home_page_controller.dart
+++ lib/home/presentation/manager/home_page_controller.dart
@@ -1,18 +1,27 @@
 import 'package:get/get.dart';
 import 'package:interview_app/home/domain/repositories/tranding_seller_repositiry.dart';
+import 'package:interview_app/home/domain/repositories/trending_product_repository.dart';
 
 class HomePageController extends GetxController {
   final ITrandingSellerRepository trandingSellerRepository;
-  HomePageController(this.trandingSellerRepository);
+  final ITrendingProductRepository trendingProductRepository;
+  HomePageController(
+      this.trandingSellerRepository, this.trendingProductRepository);
 
   @override
   void onInit() async {
     await getAllTrandingSeller();
+    await getAllTrandingProduct();
     super.onInit();
   }
 
   getAllTrandingSeller() async {
     final response = await trandingSellerRepository.getAllTrandingSeller();
     print("RES : ${response.ezShopName}");
+  }
+
+  getAllTrandingProduct() async {
+    final response = await trendingProductRepository.getAllTrendingProduct();
+    print("REP : ${response.ezShopName}");
   }
 }
